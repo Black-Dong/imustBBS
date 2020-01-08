cdd81f608aeede2a5b364e7049c9a5b6b4f27b7c
Black-Dong <1763424201@qq.com>
2020-01-08 19:46:25 +0800

    ok

--- src/main/java/top/codingdong/imustbbs/ImustBbsApplication.java
+++ src/main/java/top/codingdong/imustbbs/ImustBbsApplication.java
@@ -1,8 +1,16 @@
 package top.codingdong.imustbbs;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Dong
  * @date 2020/1/8 17:54
  */
+@SpringBootApplication
 public class ImustBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImustBbsApplication.class, args);
+    }
 }
--- src/main/java/top/codingdong/imustbbs/config/SwaggerConfig.java
+++ src/main/java/top/codingdong/imustbbs/config/SwaggerConfig.java
@@ -1,8 +1,32 @@
 package top.codingdong.imustbbs.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * @author Dong
  * @date 2020/1/8 17:56
  */
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("top.codingdong.imustbbs.controller"))
+                .paths(PathSelectors.any())
+
+                .build();
+    }
+
 }
