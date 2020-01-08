ee4174478fd28dfef5c2371ffc517051bcddec82
Black-Dong <1763424201@qq.com>
2020-01-08 19:17:10 +0800

    集成Swagger

---
+++ src/main/java/top/codingdong/imustbbs/ImustBbsApplication.java
@@ -0,0 +1,8 @@
+package top.codingdong.imustbbs;
+
+/**
+ * @author Dong
+ * @date 2020/1/8 17:54
+ */
+public class ImustBbsApplication {
+}
---
+++ src/main/java/top/codingdong/imustbbs/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package top.codingdong.imustbbs.config;
+
+/**
+ * @author Dong
+ * @date 2020/1/8 17:56
+ */
+public class SwaggerConfig {
+}
