dda6e806b7e866fa7dee8e1c5f7c882a14ccd63d
Black-Dong <1763424201@qq.com>
2020-02-26 12:36:32 +0800

    update pom

--- src/main/java/top/codingdong/imustbbs/ImustBBSApplication.java
+++ src/main/java/top/codingdong/imustbbs/ImustBBSApplication.java
@@ -2,14 +2,12 @@ package top.codingdong.imustbbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author Dong
  * @date 2020/1/8 17:54
  */
 @SpringBootApplication
-@MapperScan("top.codingdong.imustbbs.mapper")
 public class ImustBBSApplication {
 
     public static void main(String[] args) {
